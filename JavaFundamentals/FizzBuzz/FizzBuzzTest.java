857785a6112337083f1a9c3e68a224c196bcf463
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-10-24 16:07:56 -0500

    String Man

--- JavaFundamentals/FizzBuzz/FizzBuzzTest.java
+++ JavaFundamentals/FizzBuzz/FizzBuzzTest.java
@@ -1,6 +1,5 @@
 public class FizzBuzzTest {
     public static void main(String[] args) {
-        FizzBuzz fB = new FizzBuzz();
         for (int i = 0; i < 100; i++) {
             String result = FizzBuzz.fizzBuzz(i);
             String output = String.format("Number: %d :: Result: %s", i, result);
