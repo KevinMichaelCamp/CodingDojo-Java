48628bd0ab18ab73734e2d90bdcc5fb5c86017a9
KevinMichaelCamp <kevinmichaelcamp@gmail.com>
2019-10-31 16:33:23 -0500

    Spring

---
+++ Spring/strings/src/main/java/com/codingdojo/strings/StringsApplication.java
@@ -0,0 +1,27 @@
+package com.codingdojo.strings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@RestController
+
+public class StringsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringsApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String helllo() {
+		return "Hello client! How are you doing?";
+	}
+	
+	@RequestMapping("/random")
+	public String random() {
+		return "Spring Boot is great! So easy to respond with strings.";
+	}
+}
